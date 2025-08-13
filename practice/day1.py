4ab79323f5e8daea333d803e8310c99313cfd29d
simply_anhadh <anhadh34verma@gmail.com>
2025-08-13 16:08:02 +0530

    day 1

---
+++ practice/day1.py
@@ -0,0 +1,10 @@
+print ("Hello, World!")
+print ("hi im Anhadh Verma and im starting my journey of python programming and this is my first commit")
+
+#so my day one journey begins 
+
+#1st sum of declared variables
+a = 17
+b = 45 
+sum = a+b
+print (sum)
